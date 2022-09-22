98b866cb76919a6c45118eed3978705c3fb46a5d
GyZsofi <zsofia.gyori00@gmail.com>
2022-09-22 13:58:13 +0200

    basic design and init method

--- app/src/main/java/com/example/android_09_22/MainActivity.java
+++ app/src/main/java/com/example/android_09_22/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.android_09_22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText input;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private EditText output;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void init()
+    {
+        input = findViewById(R.id.input);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
     }
 }
